c30b5f292b0be7e362f3f65f2d0d206d92291eb4
Lauren Benson <lvbenson@iu.edu>
2019-11-18 14:39:08 -0500

    Show variance per connection

--- sim_CTRNN_std_repetitions.py
+++ sim_CTRNN_std_repetitions.py
@@ -14,45 +14,43 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 #Global parameters
-size = 20
-duration = 30
+size = 3
+duration = 50
 stepsize = 0.1
-repetitions = 100
 
 
 #First, show std for 0 connection.
+possible_connections = np.arange(0,size**2,2)
+possible_connections_ordered = np.flip(possible_connections)
+for c in possible_connections_ordered:
+    repetitions = np.arange(0,100,1)
+    TotalAvg = 0
+    EachRepOutputList = []
+    Std_Each_Rep = []
+    for r in repetitions:
+        time = np.arange(0.0,duration,stepsize)
+        nn = CTRNN_Lauren.CTRNN(size)
+        nn.randomizeParameters()
+        nn.setweightmatrix3(0)
+        nn.initializeState(np.zeros(size))
+        outputs1 = np.zeros((len(time)+1,size))
+        outputs1[0] = nn.Output
+        TimeSum = 0
+        step = 1
+        for t in time:
+            nn.step(stepsize)
+            outputs1[step] = nn.Output
+            NewOutput = np.absolute(outputs1[step] - outputs1[step-1])
+            SumNewOutput = np.sum(NewOutput)
+            TimeSum = TimeSum + SumNewOutput #sum of change in outputs
+            step = step+1
+            NewTimeSum = (TimeSum) / (duration * size) 
+            EachRepOutputList.append(NewTimeSum)
+            Std_Each_Rep.append(np.std(EachRepOutputList))
+            Errors = Std_Each_Rep/(np.sqrt(len(EachRepOutputList)))
+
+print(len(EachRepOutputList))
 
 
-TotalAvg = 0
-EachRepOutputList = []
-for r in range(repetitions):
-    time = np.arange(0.0,duration,stepsize)
-    nn = CTRNN_Lauren.CTRNN(size)
-    nn.randomizeParameters()
-    nn.setweightmatrix3(0)
-    nn.initializeState(np.zeros(size))
-    outputs1 = np.zeros((len(time)+1,size))
-    outputs1[0] = nn.Output
-    TimeSum = 0
-    step = 1
-    for t in time:
-        nn.step(stepsize)
-        outputs1[step] = nn.Output
-        NewOutput = np.absolute(outputs1[step] - outputs1[step-1])
-        SumNewOutput = np.sum(NewOutput)
-        TimeSum = TimeSum + SumNewOutput
-        step = step+1
-    NewTimeSum = (TimeSum) / (duration * size)
-    EachRepOutputList.append(NewTimeSum)
-    
 
-
-
-plt.plot(time, EachRepOutputList)
-#plt.errorbar(possible_connections, NewTotalAvgList, yerr=Errors, fmt='o')
-plt.xlabel("Number of connections")
-plt.ylabel("Change in Neural Output")
-plt.title("Neural activity")
-plt.show()        
-        
 
